9daf4111a4f61338da5278b46d7ade7e62fa228b
DemianTitov <titov.demyan@yandex.ru>
2019-11-19 13:08:26 +0300

    first commit

---
+++ Третий блок/script_3.js
@@ -0,0 +1,9 @@
+let arrA = [ 12, 4, 3, 10, 1, 20 ] ;
+let arrB = [-3, -7, -100, -33];
+let arrC = arrA.concat(arrB);
+console.log(arrC);
+
+let area = [ null, null, null, null, null, null, null, null, null ]
+
+
+let arrK = [12,4,3,10,1,20];
