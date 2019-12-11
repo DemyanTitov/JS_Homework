5d14c821dd4134ccff23a911e16057c3c386494f
DemianTitov <titov.demyan@yandex.ru>
2019-12-11 17:57:11 +0300

    Добавление 6 блока с задачами

---
+++ Шестой блок/script_6.js
@@ -0,0 +1,35 @@
+// 1. Реализовать функцию foo:
+// let a = {
+//     name: 'static',
+//     count: 0
+// }
+// console.log(foo(a, 'count', 10)); /* В консоль выведет: [{name:'static', count:0},
+//  {name:'static', count:1}, ..., {name:'static', count:9}] */
+
+let a = {
+    name: 'static',
+    count: 0
+}
+
+function foo(a, propName, num) {
+  arr = [];
+  for (var i = a.count; i < num; i++) {
+    let obj = Object.assign({}, a);
+    obj.count = i;
+    arr.push(obj);
+  }
+  return arr;
+}
+
+console.log(foo(a, 'count', 10));
+
+// 2. Сделайте функцию, каждый вызов который будет генерировать одно случайное число
+// от 1 до 100, но так, чтобы оно не повторялось, пока не будут перебраны все
+// числа из этого промежутка. Решите задачу через замыкания - в замыкании должен
+// хранится массив чисел, которые уже были сгенерированы функцией.
+
+function randomNum(min, max) {
+  let num = Math.floor(Math.random() * (max - min + 1) + min);
+  return num;
+}
+console.log(randomNum(1,100));
