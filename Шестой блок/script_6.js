8ed1be945f4453579733556cf064278bdd75e374
DemianTitov <titov.demyan@yandex.ru>
2019-12-17 17:10:05 +0300

    Все задачи.

--- Шестой блок/script_6.js
+++ Шестой блок/script_6.js
@@ -54,3 +54,57 @@ console.log(t());
 // - свойства: Имя, Фамилия, Возраст, Интересы (в виде массива), Место обучения.
 // - метод объекта выводящий в консоль биографическую справку в виде, например:
 // «Иван Петров. 21 год. Интересы: программирование, музыка, аниме. Учится в ИТМО.
+
+let objStudent = {
+  firstName: "Eric",
+  lastName: "Cartman",
+  age: 12,
+  interests: ["play games", "eat", "jokes"],
+  placeOfStuding: "School of South Park",
+  getAllInfo() {
+    console.log(this.firstName + " " + this.lastName + ". " + this.age + " год. Интересы: " + this.interests.join(", ") + ". Учится в " + this.placeOfStuding + ".")
+  }
+}
+objStudent.getAllInfo();
+
+objSt2 = objStudent;
+
+objSt2.firstName = "Петя";
+objSt2.lastName = "Иванов";
+objSt2.age = 21;
+objSt2.interests[1] = "плавать";
+objSt2.placeOfStuding = "ПТУ №9";
+
+objStudent.getAllInfo();
+
+// 4. Написать функцию вычисляющую факториал числа с использованием рекурсии.
+// Факториал числа - это число, умноженное на себя минус один, затем на себя
+// минус два и так далее, до единицы. Обозначается n!
+// Определение факториала можно записать как: n! = n * (n - 1) * (n - 2) * ...*1
+
+function factorialis(n) {
+  if (n == 1) {
+    return n;
+  } else {
+    return n * factorialis(n - 1);
+}
+}
+console.log(factorialis(5));
+
+// 5. Отсортировать массив по полю 'price' используя метод sort и передаваемую ей функцию,
+// определяющую порядок сортировки. Массив для тестирования:
+// let arr = [ { 'price' : 10, 'count' : 2 }, { 'price' : 5, 'count' : 5},
+// { 'price' : 8, 'count' : 5 }, { 'price' : 12, 'count' : 4 }, { 'price' : 8, 'count' : 4},];
+
+
+let arr2 = [
+  { 'price' : 10, 'count' : 2 },
+  { 'price' : 5, 'count' : 5},
+  { 'price' : 8, 'count' : 5 },
+  { 'price' : 12, 'count' : 4 },
+  { 'price' : 8, 'count' : 4},
+];
+
+arr2 = arr2.sort((a, b) => b.price - a.price);
+
+console.log(arr2);
